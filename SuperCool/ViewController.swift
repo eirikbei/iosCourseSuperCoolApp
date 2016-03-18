60dfe01eba83cee4530e47d45ba4b74c4775ee9a
Eirik Bo Eidsvig <eirikboeidsvig@Eiriks-MacBook-Pro.local>
2016-03-18 15:56:22 +0100

    added files for supercool

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CoolLogo: UIImageView!
+    @IBOutlet weak var CoolBG: UIImageView!
+    @IBOutlet weak var unCoolBtn: UIButton!
+    @IBOutlet weak var veryCool: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func makeMeNotSoUncool(sender: AnyObject) {
+        
+        CoolLogo.hidden = false
+        CoolBG.hidden = false
+        unCoolBtn.hidden = true
+        
+    }
 
 }
 
